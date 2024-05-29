4f52e76330a1c362346c2640811381791ac1a4d3
mbajur <mbajur@gmail.com>
2024-05-29 20:27:47 +0200

    fix

--- activity_pub/app/services/activity_pub/activity_handlers/follow_handler.rb
+++ activity_pub/app/services/activity_pub/activity_handlers/follow_handler.rb
@@ -5,7 +5,7 @@ module ActivityPub
         verify_signature!
 
         local_target_guid = ActivityPub::UriToLocalObjectIdFinder.new(body['object']).call
-        local_target = ActivityPub::Object.local.find_by(guid: local_target_guid)
+        local_target = ActivityPub::Object.local.find_by(id: local_target_guid)
         raise ActiveRecord::RecordNotFound, "Local target #{local_target_guid} not found"
 
         follow = ActivityPub::ObjectAssociation.first_or_initialize_by(type: 'follow', guid: body['id'])
