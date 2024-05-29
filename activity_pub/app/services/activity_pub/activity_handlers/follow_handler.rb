9b1d336e445429029180827043ad8062698175ca
mbajur <mbajur@gmail.com>
2024-05-29 19:42:33 +0200

    Remove NotImplementedError from Follow handler

--- activity_pub/app/services/activity_pub/activity_handlers/follow_handler.rb
+++ activity_pub/app/services/activity_pub/activity_handlers/follow_handler.rb
@@ -2,7 +2,7 @@ module ActivityPub
   module ActivityHandlers
     class FollowHandler < BaseHandler
       def call
-        raise NotImplementedError, 'Follow activity is not yet implemented!'
+        # raise NotImplementedError, 'Follow activity is not yet implemented!'
       end
     end
   end
