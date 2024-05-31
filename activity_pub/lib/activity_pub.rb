9839eb87bce213ce9475bdc62454b277573363a7
mbajur <mbajur@gmail.com>
2024-05-31 07:25:01 +0200

    fix

--- activity_pub/lib/activity_pub.rb
+++ activity_pub/lib/activity_pub.rb
@@ -1,5 +1,6 @@
 require "activity_pub/version"
 require "activity_pub/engine"
+# require 'activity_pub/http_client'
 
 require 'faraday'
 require 'faraday_middleware'
@@ -9,6 +10,14 @@ require 'good_job'
 require 'rails_api_logger'
 require 'http_signatures'
 
+# require 'http_signatures/extensions/faraday'
+
 module ActivityPub
-  # Your code goes here...
+  mattr_accessor :domain
+
+  self.domain = nil
+
+  def self.setup(&block)
+    yield self
+  end
 end
