cf6066465f335ed41c99d94559a462861ebeeebe
mbajur <mbajur@gmail.com>
2024-06-01 18:32:41 +0200

    fix

--- activity_pub/app/services/activity_pub/remote_key_refresher.rb
+++ activity_pub/app/services/activity_pub/remote_key_refresher.rb
@@ -6,7 +6,7 @@ module ActivityPub
 
     def call
       local_object = ActivityPub::ObjectEnsurer.new(@uri).call
-      remote_object = HttpClient.new(nil).get(URI.parse(local_object.guid)).body
+      remote_object = ActivityPub::HttpClient.new(nil).get(URI.parse(local_object.guid)).body
       remote_object = ActivityPub::ObjectDataSanitizer.new(remote_object).call
 
       local_object.update!(public_key: remote_object['public_key'])
--- activity_pub/lib/activity_pub.rb
+++ activity_pub/lib/activity_pub.rb
@@ -1,6 +1,6 @@
 require "activity_pub/version"
 require "activity_pub/engine"
-# require 'activity_pub/http_client'
+require 'activity_pub/http_client'
 
 require 'faraday'
 require 'faraday_middleware'
@@ -10,7 +10,7 @@ require 'good_job'
 require 'rails_api_logger'
 require 'http_signatures'
 
-# require 'http_signatures/extensions/faraday'
+require 'http_signatures/extensions/faraday'
 
 module ActivityPub
   mattr_accessor :domain
