6e4fa94f664f49d11b43487c03b1010c5cb5e3e5
mbajur <mbajur@gmail.com>
2024-06-18 09:58:22 +0200

    Like model fix

--- activity_pub/app/models/activity_pub/like.rb
+++ activity_pub/app/models/activity_pub/like.rb
@@ -1,4 +1,4 @@
 class ActivityPub::Like < ApplicationRecord
-  belongs_to :source_ap_object, class_name: 'Ap::Object'
-  belongs_to :target_ap_object, class_name: 'Ap::Object'
+  belongs_to :source_ap_object, class_name: 'ActivityPub::Object'
+  belongs_to :target_ap_object, class_name: 'ActivityPub::Object'
 end
