8662f108e69202ec3cd0d1931522ce6ab1473fdb
mbajur <mbajur@gmail.com>
2025-04-01 08:19:42 +0200

    Rename content attribute to content_raw in ArticleResource for improved data handling

--- activity_pub/app/resources/activity_pub/article_resource.rb
+++ activity_pub/app/resources/activity_pub/article_resource.rb
@@ -10,7 +10,7 @@ module ActivityPub
     has_followers false
 
     register_data_attribute :name
-    register_data_attribute :content
+    register_data_attribute :content_raw
     register_data_attribute :url
     register_data_attribute :published_at
 
