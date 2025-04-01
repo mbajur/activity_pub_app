93c2d697a56aca09af12c4e6ac7bd773c82147fa
mbajur <mbajur@gmail.com>
2025-04-01 17:49:53 +0200

    Temporarily comment out shrine storages configuration

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,10 @@
 require 'shrine'
 require 'shrine/storage/file_system'
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads'),       # permanent
-}
+# Shrine.storages = {
+#   cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
+#   store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads'),       # permanent
+# }
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays
