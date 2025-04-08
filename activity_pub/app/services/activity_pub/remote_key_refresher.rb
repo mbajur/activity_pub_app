654184619158773da7620c2df341ba7cf6f83a5f
mbajur <mbajur@gmail.com>
2025-04-08 19:42:45 +0200

    Update public key assignment in remote key refresher to use data attribute and save changes

--- activity_pub/app/services/activity_pub/remote_key_refresher.rb
+++ activity_pub/app/services/activity_pub/remote_key_refresher.rb
@@ -9,7 +9,8 @@ module ActivityPub
       remote_object = ActivityPub::HttpClient.new(nil).get(URI.parse(local_object.guid)).body
       remote_object = ActivityPub::ObjectDataSanitizer.new(remote_object).call
 
-      local_object.update!(public_key: remote_object['public_key'])
+      local_object.data.public_key = remote_object['public_key']
+      local_object.save!
 
       local_object
     end
