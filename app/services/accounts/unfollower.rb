1f62d22d199adbb47f8760821b7071f1d753aad4
mbajur <mbajur@gmail.com>
2025-04-08 20:30:07 +0200

    Add distribution step to unfollower for federating follow activities

--- app/services/accounts/unfollower.rb
+++ app/services/accounts/unfollower.rb
@@ -5,6 +5,7 @@ module Accounts
 
     step :find_follow
     step :mark_as_undoed
+    step :distribute
 
     output :follow
 
@@ -12,6 +13,11 @@ module Accounts
 
     def find_follow
       self.follow = ActivityPub::Follow.find_by(source_ap_object: actor, target_ap_object: target)
+    end
+
+    def distribute
+      activity = ActivityPub::FollowSerializer.new(follow, with_context: true)
+      ActivityPub::FederateObjectJob.perform_later(actor, target.data.inbox, activity.to_json)
     end
 
     def mark_as_undoed
