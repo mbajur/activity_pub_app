23bbd75574361a010389d27b25ad3f0d05bf7039
mbajur <mbajur@gmail.com>
2025-04-09 09:13:22 +0200

    Add distribution step to unliker service for remote targets

--- app/services/objects/unliker.rb
+++ app/services/objects/unliker.rb
@@ -4,15 +4,27 @@ module Objects
     arg :target
 
     step :remove_like
+    step :distribute
+
+    output :like
 
     private
 
     def remove_like
-      like = ActivityPub::Like.find_by(
+      self.like = ActivityPub::Like.find_by(
         source_ap_object: actor,
         target_ap_object: target
       )
       like&.destroy
+    end
+
+    def distribute
+      if target.remote?
+        activity = ActivityPub::UndoSerializer.new(like, with_context: true)
+        target.attributed_to.each do |target_actor|
+          ActivityPub::FederateObjectJob.perform_later(actor, target_actor.data.inbox, activity.to_json)
+        end
+      end
     end
   end
 end
